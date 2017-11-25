4cc66f68d225ade8b211d17eef35764ba8448dc8
Viktharien <viktharien@zoho.com>
2017-11-25 07:09:23 +0000

    fixed reference error, but need some mad touch up

--- client/src/components/elements/displayform.js
+++ client/src/components/elements/displayform.js
@@ -5,24 +5,25 @@ import DisplayButton from './displaybutton';
 class DisplayForm extends Component {
     constructor(props) {
         super(props);
+        this.el;
         this.element;
         this.stateRef = this.props.state;
         this.inputs = this.stateRef.inputs;
         this.syncDef = this.syncDef.bind(this);
         this.syncFoc = this.syncFoc.bind(this);
-        this.el = this.props.id.split('-');
-        
+        this.parts = ['display-form', 'display-input', 'display-button'];
+
         this.onClick = this.onClick.bind(this);
 
     }
 
     syncDef() {
-        console.log(this.el);
-        this.element = document.getElementById(this.el.join('-'));
-        console.log(this.element);
         this.stateRef = this.props.state;
         let dur = this.stateRef.transitionDur;
-        this.element.setAttribute('style', `
+        this.parts.forEach((item) => {
+            this.element = document.getElementById(item);
+            this.el = item.split('-');
+            this.element.setAttribute('style', `
             cursor: pointer;
             border: none;
             -webkit-transition: all ${dur};
@@ -30,16 +31,23 @@ class DisplayForm extends Component {
             -o-transition: all ${dur};
             transition: all ${dur};
         `);
-        this.element.style.borderRadius = this.stateRef[this.el[1] + 'BorderRadius'];
-        this.element.style.color = this.stateRef[this.el[1] + 'Color'];
-        this.element.style.backgroundColor = this.stateRef[this.el[1] + 'Bg'];
-        this.element.style.height = this.stateRef[this.el[1] + 'Ht'];
-        this.element.style.width = this.stateRef[this.el[1] + 'Wt'];
+            this.element.style.borderRadius = this.stateRef[this.el[1] + 'BorderRadius'];
+            this.element.style.color = this.stateRef[this.el[1] + 'Color'];
+            this.element.style.backgroundColor = this.stateRef[this.el[1] + 'Bg'];
+            this.element.style.height = this.stateRef[this.el[1] + 'Ht'];
+            this.element.style.width = this.stateRef[this.el[1] + 'Wt'];
+        });
+
     }
 
     syncFoc() {
-        this.element.style.color = this.stateRef[this.el[1] + 'FocColor'];
-        this.element.style.backgroundColor = this.stateRef[this.el[1] + 'FocBg'];
+        this.parts.forEach((item)=>{
+            this.element = document.getElementById(item);
+            this.el = item.split('-');
+            this.element.style.color = this.stateRef[this.el[1] + 'FocColor'];
+            this.element.style.backgroundColor = this.stateRef[this.el[1] + 'FocBg'];
+        });
+        
     }
 
 
@@ -52,29 +60,27 @@ class DisplayForm extends Component {
         this.props.onAdd(e);
     }
 
-
     componentDidMount() {
         this.syncDef();
     }
 
     componentDidUpdate() {
         this.syncDef();
-
     }
     render() {
         return (
             <div>
             <h4>Add a field: <button name='addInput' value='add' onClick={this.onClick} >+</button><button name='delInput' value='del' onClick={this.onClick} >-</button></h4>
-            <form action=''>
+            <form id='display-form' action=''>
             
                 {this.inputs.map((key)=>{
                    return (
                     
-                    <p key={key}><input type='text' id={'display-input'} placeholder="What kind of field is this?"/></p>
+                    <p key={key}><input type='text' id='display-input' placeholder="What kind of field is this?"/></p>
                     
                    );
                 })}
-                <DisplayButton id={'display-button'} onClick={this.props.onClick} value={'Output.click'} state={this.props.state} text={'Click me to generate your code!'} />
+                <DisplayButton id='display-button' onClick={this.props.onClick} value={'Output.click'} state={this.props.state} text={'Click me to generate your code!'} />
             </form>
             </div>
         );
