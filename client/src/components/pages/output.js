307432e58e495613a5a98df438d867a6317680d7
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-11-26 12:13:35 -0600

    set up form code generation

--- client/src/components/pages/output.js
+++ client/src/components/pages/output.js
@@ -1,126 +1,129 @@
-import React from 'react';
+import React, { Component } from "react";
 
-import DisplayButton from '../elements/displaybutton';
+import DisplayButton from "../elements/displaybutton";
 
-const Output = (props) => {
-    let state = props.state;
-    let strOutput = `
-import React, { Component } from 'react';
 
-class Button extends Component {
+class Output extends Component {
   constructor(props) {
     super(props);
-    this.id=this.props.id;
-    this.el = null;
-    this.active = 0;
-    this.style = {
-      default: \`
-        height: ${state.defHt};
-        width: ${state.defWt};
-        color: ${state.defColor};
-        background-color: ${state.defBg};
-        cursor: pointer;
-        border: none;
-        border-radius: ${state.borderRadius};
-        \`,
-      render: null,
-      hover: \`
-      color: ${state.hovColor};
-      background-color: ${state.hovBg};
-      \`,
-      focus: \`
-      color: ${state.focColor}
-      background-color: ${state.focBg};\`,
-      transitions: \`
-        -webkit-transition: all ${state.transitionDur};
-        -moz-transition: all ${state.transitionDur};
-        -o-transition: all ${state.transitionDur};
-        transition: all ${state.transitionDur};
-       \`
-    };
-    
-    this.onClick = this.onClick.bind(this);
-    this.onMouseEnter = this.onMouseEnter.bind(this);
-    this.onMouseLeave = this.onMouseLeave.bind(this);
-    this.onFocus = this.onFocus.bind(this);
-    this.setStyle = this.setStyle.bind(this);
-    this.resetStyle = this.resetStyle.bind(this);
-  }
-  onClick(e) {
-    this.props.onClick(e.target.value);
-  }
-  onMouseEnter() {
-    this.active === 0 && this.setStyle(this.style.hover);
-    
-  }
-  onMouseLeave() {
-    this.setStyle(this.style.render);
-  }
-  onFocus() {
-    this.resetStyle();
-    this.style.render += this.style.focus;
-    this.setStyle(this.style.render);
-    this.active = 1;
-  }
-  setStyle(val) {
-    switch(val) {
-      case this.style.render:
-        this.el.setAttribute("style",this.style.render);
-        break;
-      case this.style.default:
-        this.el.setAttribute("style",this.style.default);
-        break;
-      default:
-        this.el.setAttribute("style",this.style.render + val);
-        break;
+
+    this.state = this.props.state;
+    this.inputString = '';
+    this.inputs = this.state.inputs.map((key)=>{
+      this.inputString += `\n<Input id={'input'+${key}} onChange={this.onChange}/>`;
+    });
+    this.strOutput =  `
+    import React, { Component } from 'react';
+
+    import Input from './input';
+    import TextArea from './textarea';
+    import Button from './button';
+
+    class Form extends Component {
+      constructor(props) {
+        super(props);
+        this.id=this.props.id;
+        this.el = null;
+        this.active = 0;
+        this.style = {
+          default: \`
+            height: ${this.state.formHt};
+            width: ${this.state.formWt};
+            color: ${this.state.formColor};
+            background-color: ${this.state.formBg};
+            border: none;
+            border-radius: ${this.state.formBorderRadius};
+            \`,
+          render: null,
+          hover: null,
+          focus: \`
+          color: ${this.state.formFocColor}
+          background-color: ${this.state.formFocBg};\`,
+          transitions: \`
+            -webkit-transition: all ${this.state.transitionDur};
+            -moz-transition: all ${this.state.transitionDur};
+            -o-transition: all ${this.state.transitionDur};
+            transition: all ${this.state.transitionDur};
+           \`
+        };
+        
+        this.onFocus = this.onFocus.bind(this);
+        this.setStyle = this.setStyle.bind(this);
+        this.resetStyle = this.resetStyle.bind(this);
+        
+        this.onChange = this.onChange.bind(this);
+        this.onClick = this.onClick.bind(this);
+      }
+      onClick(e) {
+        
+      }
+      onFocus() {
+        this.resetStyle();
+        this.style.render += this.style.focus;
+        this.setStyle(this.style.render);
+        this.active = 1;
+      }
+      setStyle(val) {
+        switch(val) {
+          case this.style.render:
+            this.el.setAttribute("style",this.style.render);
+            break;
+          case this.style.default:
+            this.el.setAttribute("style",this.style.default);
+            break;
+          default:
+            this.el.setAttribute("style",this.style.render + val);
+            break;
+        }
+        
+       }
+      resetStyle() {
+        this.active = 0;
+        this.style.render = this.style.default;
+        this.setStyle(this.style.default);//sets style to default
+      }
+      
+      componentDidMount() {
+        this.el = document.getElementById(this.id);
+        this.style.default += this.style.transitions;
+        this.style.render = this.style.default;//load transitions if any
+        this.setStyle(this.style.render);
+      }
+      render() {
+        return (
+          ${this.inputString}
+          <TextArea />
+          <Button />
+        );
+      }
     }
     
-   }
-  resetStyle() {
-    this.active = 0;
-    this.style.render = this.style.default;
-    this.setStyle(this.style.default);//sets style to default
-  }
-  
-  componentDidMount() {
-    this.el = document.getElementById(this.id);
-    this.style.default += this.style.transitions;
-    this.style.render = this.style.default;//load transitions if any
-    this.setStyle(this.style.render);
+    export default Form;
+    
+        `;
   }
   render() {
     return (
-      <button
-        id={this.id}
-        value={this.props.value}
-        onClick={this.onClick}
-        onMouseEnter={this.onMouseEnter}
-        onMouseLeave={this.onMouseLeave}
-        onFocus={this.onFocus}
-        onBlur={this.resetStyle}
-      >
-        {this.props.text}
-      </button>
-    );
-  }
-}
-
-export default Button;
-
-    `
-    return (
-        <div id='page-output' className="page">
-            <div id='output-container'>
-                <h3>Here's your button!</h3>
-                <DisplayButton id={'output-button'} onClick={props.pageHandler} value={'Download.dl'} content={strOutput} state={props.state} text={'Download me!'} />
-                <h3>Here's your button code!</h3>
-                <hr/>
-                <div><pre>{strOutput}</pre></div>
-                
-            </div>
+      <div id="page-output" className="page">
+        <div id="output-container">
+          <h3>Here's your button!</h3>
+          <DisplayButton
+            id={"output-button"}
+            onClick={this.props.pageHandler}
+            value={"Download.dl"}
+            content={this.strOutput}
+            state={this.props.state}
+            text={"Download me!"}
+          />
+          <h3>Here's your button code!</h3>
+          <hr />
+          <div>
+            <pre>{this.strOutput}</pre>
+          </div>
+        </div>
       </div>
     );
-
+  }
 }
 
 export default Output;
---
+++ client/src/components/templates/template-button.js
@@ -0,0 +1,105 @@
+let templateButton = `
+import React, { Component } from 'react';
+
+class Button extends Component {
+  constructor(props) {
+    super(props);
+    this.id=this.props.id;
+    this.el = null;
+    this.active = 0;
+    this.style = {
+      default: \`
+        height: ${state.defHt};
+        width: ${state.defWt};
+        color: ${state.defColor};
+        background-color: ${state.defBg};
+        cursor: pointer;
+        border: none;
+        border-radius: ${state.borderRadius};
+        \`,
+      render: null,
+      hover: \`
+      color: ${state.hovColor};
+      background-color: ${state.hovBg};
+      \`,
+      focus: \`
+      color: ${state.focColor}
+      background-color: ${state.focBg};\`,
+      transitions: \`
+        -webkit-transition: all ${state.transitionDur};
+        -moz-transition: all ${state.transitionDur};
+        -o-transition: all ${state.transitionDur};
+        transition: all ${state.transitionDur};
+       \`
+    };
+    
+    this.onClick = this.onClick.bind(this);
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+    this.onFocus = this.onFocus.bind(this);
+    this.setStyle = this.setStyle.bind(this);
+    this.resetStyle = this.resetStyle.bind(this);
+  }
+  onClick(e) {
+    this.props.onClick(e.target.value);
+  }
+  onMouseEnter() {
+    this.active === 0 && this.setStyle(this.style.hover);
+    
+  }
+  onMouseLeave() {
+    this.setStyle(this.style.render);
+  }
+  onFocus() {
+    this.resetStyle();
+    this.style.render += this.style.focus;
+    this.setStyle(this.style.render);
+    this.active = 1;
+  }
+  setStyle(val) {
+    switch(val) {
+      case this.style.render:
+        this.el.setAttribute("style",this.style.render);
+        break;
+      case this.style.default:
+        this.el.setAttribute("style",this.style.default);
+        break;
+      default:
+        this.el.setAttribute("style",this.style.render + val);
+        break;
+    }
+    
+   }
+  resetStyle() {
+    this.active = 0;
+    this.style.render = this.style.default;
+    this.setStyle(this.style.default);//sets style to default
+  }
+  
+  componentDidMount() {
+    this.el = document.getElementById(this.id);
+    this.style.default += this.style.transitions;
+    this.style.render = this.style.default;//load transitions if any
+    this.setStyle(this.style.render);
+  }
+  render() {
+    return (
+      <button
+        id={this.id}
+        value={this.props.value}
+        onClick={this.onClick}
+        onMouseEnter={this.onMouseEnter}
+        onMouseLeave={this.onMouseLeave}
+        onFocus={this.onFocus}
+        onBlur={this.resetStyle}
+      >
+        {this.props.text}
+      </button>
+    );
+  }
+}
+
+export default Button;
+
+    `;
+export default templateButton;
