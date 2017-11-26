7d1a3f07ca3cbbf6e4bdc4af45dfec7ea5547296
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-11-26 11:28:04 -0600

    cleanup server

--- server/index.js
+++ server/index.js
@@ -4,16 +4,12 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Priority serve any static files.
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// Answer API requests.
 app.get('/api', function (req, res) {
   res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
 });
 
-// All remaining requests return the React app, so it can handle routing.
 app.get('*', function(request, response) {
   response.sendfile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
