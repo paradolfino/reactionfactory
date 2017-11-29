e3ed70a0f2fdf3df7029d6063aeddb898e5772cd
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-11-28 21:11:07 -0600

    start work on templates functions for use in output view

---
+++ client/src/components/templates/templates.js
@@ -0,0 +1,200 @@
+const temp_form = (state) => {
+    return (`
+    import React, { Component } from 'react';
+
+    import Input from './input';
+    import TextArea from './textarea';
+    import Button from './button';
+
+    class Form extends Component {
+      constructor(props) {
+        super(props);
+        this.id=this.props.id;
+        this.el = null;
+        this.active = 0;
+        this.style = {
+          default: \`
+            height: ${state.formHt};
+            width: ${state.formWt};
+            color: ${state.formColor};
+            background-color: ${state.formBg};
+            border: none;
+            border-radius: ${state.formBorderRadius};
+            \`,
+          render: null,
+          hover: null,
+          focus: \`
+          color: ${state.formFocColor}
+          background-color: ${state.formFocBg};\`,
+          transitions: \`
+            -webkit-transition: all ${state.transitionDur};
+            -moz-transition: all ${state.transitionDur};
+            -o-transition: all ${state.transitionDur};
+            transition: all ${state.transitionDur};
+           \`
+        };
+        
+        this.onFocus = this.onFocus.bind(this);
+        this.setStyle = this.setStyle.bind(this);
+        this.resetStyle = this.resetStyle.bind(this);
+        
+        this.onChange = this.onChange.bind(this);
+        this.onClick = this.onClick.bind(this);
+      }
+      onClick(e) {
+        
+      }
+      onFocus() {
+        this.resetStyle();
+        this.style.render += this.style.focus;
+        this.setStyle(this.style.render);
+        this.active = 1;
+      }
+      setStyle(val) {
+        switch(val) {
+          case this.style.render:
+            this.el.setAttribute("style",this.style.render);
+            break;
+          case this.style.default:
+            this.el.setAttribute("style",this.style.default);
+            break;
+          default:
+            this.el.setAttribute("style",this.style.render + val);
+            break;
+        }
+        
+       }
+      resetStyle() {
+        this.active = 0;
+        this.style.render = this.style.default;
+        this.setStyle(this.style.default);//sets style to default
+      }
+      
+      componentDidMount() {
+        this.el = document.getElementById(this.id);
+        this.style.default += this.style.transitions;
+        this.style.render = this.style.default;//load transitions if any
+        this.setStyle(this.style.render);
+      }
+      render() {
+        return (
+          ${this.inputString}
+          <TextArea />
+          <Button />
+        );
+      }
+    }
+    
+    export default Form;
+    
+        `);
+}
+
+const temp_button = (state) => {
+    return (`
+    import React, { Component } from 'react';
+    
+    class Button extends Component {
+      constructor(props) {
+        super(props);
+        this.id=this.props.id;
+        this.el = null;
+        this.active = 0;
+        this.style = {
+          default: \`
+            height: ${state.defHt};
+            width: ${state.defWt};
+            color: ${state.defColor};
+            background-color: ${state.defBg};
+            cursor: pointer;
+            border: none;
+            border-radius: ${state.borderRadius};
+            \`,
+          render: null,
+          hover: \`
+          color: ${state.hovColor};
+          background-color: ${state.hovBg};
+          \`,
+          focus: \`
+          color: ${state.focColor}
+          background-color: ${state.focBg};\`,
+          transitions: \`
+            -webkit-transition: all ${state.transitionDur};
+            -moz-transition: all ${state.transitionDur};
+            -o-transition: all ${state.transitionDur};
+            transition: all ${state.transitionDur};
+           \`
+        };
+        
+        this.onClick = this.onClick.bind(this);
+        this.onMouseEnter = this.onMouseEnter.bind(this);
+        this.onMouseLeave = this.onMouseLeave.bind(this);
+        this.onFocus = this.onFocus.bind(this);
+        this.setStyle = this.setStyle.bind(this);
+        this.resetStyle = this.resetStyle.bind(this);
+      }
+      onClick(e) {
+        this.props.onClick(e.target.value);
+      }
+      onMouseEnter() {
+        this.active === 0 && this.setStyle(this.style.hover);
+        
+      }
+      onMouseLeave() {
+        this.setStyle(this.style.render);
+      }
+      onFocus() {
+        this.resetStyle();
+        this.style.render += this.style.focus;
+        this.setStyle(this.style.render);
+        this.active = 1;
+      }
+      setStyle(val) {
+        switch(val) {
+          case this.style.render:
+            this.el.setAttribute("style",this.style.render);
+            break;
+          case this.style.default:
+            this.el.setAttribute("style",this.style.default);
+            break;
+          default:
+            this.el.setAttribute("style",this.style.render + val);
+            break;
+        }
+        
+       }
+      resetStyle() {
+        this.active = 0;
+        this.style.render = this.style.default;
+        this.setStyle(this.style.default);//sets style to default
+      }
+      
+      componentDidMount() {
+        this.el = document.getElementById(this.id);
+        this.style.default += this.style.transitions;
+        this.style.render = this.style.default;//load transitions if any
+        this.setStyle(this.style.render);
+      }
+      render() {
+        return (
+          <button
+            id={this.id}
+            value={this.props.value}
+            onClick={this.onClick}
+            onMouseEnter={this.onMouseEnter}
+            onMouseLeave={this.onMouseLeave}
+            onFocus={this.onFocus}
+            onBlur={this.resetStyle}
+          >
+            {this.props.text}
+          </button>
+        );
+      }
+    }
+    
+    export default Button;
+    
+        `);
+}
+
+export {temp_form, temp_button};
