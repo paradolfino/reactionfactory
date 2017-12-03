b333dbd426a2a625c7727d08a08b9e7f9c4b21e9
Aaron Viktharien Volander Cil <viktharien@zoho.com>
2017-12-03 17:25:07 -0600

    refactor buttonfactory to similar format of formfactory

--- client/src/components/pages/buttonfactory.js
+++ client/src/components/pages/buttonfactory.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import Form from '../elements/factory-form-button';
-import DisplayButton from '../elements/displaybutton';
+import React, { Component } from "react";
+import Form from "../elements/factory-form-button";
+import DisplayButton from "../elements/displaybutton";
 
-const ButtonFactory = (props) => {
+class ButtonFactory extends Component {
+  constructor(props) {
+    super(props);
+    this.outputType = "temp_button";
+  }
 
+  componentDidMount() {
+    this.props.outputHandler(this.outputType);
+  }
+
+  render() {
     return (
-        <div id='page-factory' className="page">
-            <div>
-                <Form onChange={props.inputHandler}/>
-                <DisplayButton id={'display-button'} onClick={props.pageHandler} value={'Output.click'} state={props.state} text={'Click me to generate your code!'} outputType='temp_button' />
-            </div>
+      <div id="page-factory" className="page">
+        <div>
+          <Form onChange={this.props.inputHandler} />
+          <DisplayButton
+            id={"display-button"}
+            onClick={this.props.pageHandler}
+            value={"Output.click"}
+            state={this.props.state}
+            text={"Click me to generate your code!"}
+            outputType={this.outputType}
+          />
+        </div>
       </div>
     );
-
+  }
 }
 
 export default ButtonFactory;
